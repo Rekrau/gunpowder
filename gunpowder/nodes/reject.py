6e71aebe1fd0b0fca386c554ea6f86e00f69e217
rekrau <renate.b.krause@bluewin.ch>
2018-02-08 08:44:34 -0500

    Update Reject node to version which is working correctly

--- gunpowder/nodes/reject.py
+++ gunpowder/nodes/reject.py
@@ -1,16 +1,28 @@
 import logging
+import random
 
 from .batch_filter import BatchFilter
 from gunpowder.profiling import Timing
 from gunpowder.volume import VolumeTypes
-import numpy as np
-
 
 logger = logging.getLogger(__name__)
 
 class Reject(BatchFilter):
+    '''Reject batches based on the masked-in vs. masked-out ratio.
 
-    def __init__(self, min_masked=0.5, mask_volume_type=VolumeTypes.GT_MASK, reject_probability=0.05):
+    Args:
+
+        min_masked(float, optional): The minimal required ratio of masked-in
+            vs. masked-out voxels. Defaults to 0.5.
+
+        mask_volume_type(:class:``VolumeTypes``): The mask to use.
+
+        reject_probability(float, optional): The probability by which a batch
+            that is not valid (less than min_masked) is actually rejected.
+            Defaults to 1., i.e. strict rejection.
+    '''
+
+    def __init__(self, min_masked=0.5, mask_volume_type=VolumeTypes.GT_MASK, reject_probability=1.):
         self.min_masked = min_masked
         self.mask_volume_type = mask_volume_type
         self.reject_probability = reject_probability
@@ -30,16 +42,20 @@ class Reject(BatchFilter):
         assert self.mask_volume_type in request, "Reject can only be used if a GT mask is requested"
 
         have_good_batch = False
-        accept_even_if_empty = False
-
-        while not (have_good_batch or accept_even_if_empty):
+        while not have_good_batch:
 
             batch = self.upstream_provider.request_batch(request)
             mask_ratio = batch.volumes[self.mask_volume_type].data.mean()
             have_good_batch = mask_ratio>=self.min_masked
-            accept_even_if_empty = (np.random.rand() <= self.reject_probability )
 
-            if not (have_good_batch or accept_even_if_empty):
+            if not have_good_batch and self.reject_probability < 1.:
+                have_good_batch = random.random() > self.reject_probability
+                logger.debug(
+                    "accepted batch with mask ratio %f at" %mask_ratio +
+                    str(batch.volumes[self.mask_volume_type].spec.roi))
+
+            if not have_good_batch:
+
                 logger.warning(
                     "reject batch with mask ratio %f at "%mask_ratio +
                     str(batch.volumes[self.mask_volume_type].spec.roi))
